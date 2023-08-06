8854e192c4e515095852f10593170be908309266
Utkarshvr <utkarshv995@gmail.com>
2023-08-06 17:07:16 +0530

    first commit

---
+++ client/src/Chat.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+export default function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+
+    if (currentMessage.length !== 0) {
+      const message = {
+        room,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send-message", message);
+      setMessageList((list) => [...list, message]);
+      setCurrentMessage("");
+    }
+  };
+  useEffect(() => {
+    socket.on("recieve-message", (data) => {
+      console.log(data);
+      setMessageList((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <div className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <form onSubmit={sendMessage} className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Hey..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button type="submit">&#9658;</button>
+      </form>
+    </div>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const app = require("express")();
+
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("User Connected ", socket.id);
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`User with ID: ${socket.id} joined room: ${room}`);
+  });
+
+  socket.on("send-message", (msg) => {
+    console.log({ msg });
+    socket.to(msg.room).emit("recieve-message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.io);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
